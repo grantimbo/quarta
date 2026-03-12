aebcc27562da2e4d4e86e891b8072ec1a46ddcac
Grant Imbo <grant.imbo@gmail.com>
2026-03-13 04:44:04 +0800

    fixing deployment bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+console.log(
+  "NEXT_PUBLIC_FIREBASE_APIKEY in Next:",
+  process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+);
