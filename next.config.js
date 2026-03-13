6c10d68fe08ffddc11a4da810bb2729a6fd09884
Grant Imbo <grant.imbo@gmail.com>
2026-03-13 15:32:34 +0800

    setup deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
-
-console.log(
-  "NEXT_PUBLIC_FIREBASE_APIKEY in Next:",
-  process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-);
